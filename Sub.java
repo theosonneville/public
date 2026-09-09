a70839fa90d4a7ccdfc73f0477da5cccb65fa6b0
arthurodt <arthuroudinet254@gmail.com>
2026-09-09 13:28:23 +0200

    ajout de classe sub + test

---
+++ Sub.java
@@ -0,0 +1,9 @@
+
+
+
+public class Sub{
+    
+    public int sub(int a, int b){
+        return a - b;
+    }
+}
---
+++ SubTest.java
@@ -0,0 +1,9 @@
+public class SubTest {
+
+    public static void main(String[] args) {
+        Sub calc = new Sub();
+        if (calc.sub(5, 2) == 3) {
+            System.out.println("program ok");
+        }
+    }
+}
